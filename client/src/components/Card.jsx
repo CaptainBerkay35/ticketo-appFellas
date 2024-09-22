3cf98b515126dae1e614c5c28a72f8f0eb4c7695
Berkay Kaptan <kaptanberkay35@hotmail.com>
2024-09-22 14:24:14 +0300

    Modal and Inner pages added

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,6 +1,8 @@
-export default function Card({ imgSrc, title, logoSrc }) {
+import { Link } from "react-router-dom";
+
+export default function Card({ imgSrc, title, logoSrc, linkTo }) {
   return (
-    <div className="relative bg-white shadow-lg rounded-lg overflow-hidden">
+    <Link to={linkTo} className="relative bg-white shadow-lg rounded-lg overflow-hidden block">
       <img src={imgSrc} alt="Card Image" className="w-full h-48 object-cover" />
       <div className="absolute bottom-0 left-0 px-4 py-2 w-full z-10 bg-gradient-to-t from-black/50 to-transparent">
         {typeof logoSrc === "string" ? (
@@ -10,6 +12,6 @@ export default function Card({ imgSrc, title, logoSrc }) {
         )}
         <h2 className="text-xl font-bold text-white">{title}</h2>
       </div>
-    </div>
+    </Link>
   );
 }
